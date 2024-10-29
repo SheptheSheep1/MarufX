c4a77fecd199f958f8d64f415234e95d548e90f5
Ibrahim Safi <35405069+SheptheSheep1@users.noreply.github.com>
2024-10-29 16:55:04 -0700

    parsing csv ready for implementation for calcMethods

--- src/main/java/CalcMethod.java
+++ src/main/java/CalcMethod.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-class CalcMethod {
+public class CalcMethod {
     private String name;
     private double fajrAngle;
     private double ishaAngle;
@@ -12,17 +12,25 @@ class CalcMethod {
         this.ishaAngle = ishaAngle;
         this.fixed = fixed;
         this.description = description;
-    }/*
+    }
+    public CalcMethod(String[] stringArray){
+        this.name = stringArray[1];
+        this.description = stringArray[0];
+        this.fajrAngle = Double.parseDouble(stringArray[2]);
+        this.ishaAngle = Double.parseDouble(stringArray[3]);
+        if(stringArray[4].equalsIgnoreCase("Y")){this.fixed = true;}
+        else if (stringArray[4].equalsIgnoreCase("N")){this.fixed = false;}
+    }
     public CalcMethod() {
         this.name = "MWL";
         this.fajrAngle = 18.0;
         this.ishaAngle = 17.0;
         this.fixed = false;
         this.description = "Muslim World League";
-    }*/
+    }
 
     public String toString(){
-        return String.format("%s Fajr Angle: %f Isha Angle: %f", description, fajrAngle, ishaAngle);
+        return String.format("%s, Fajr Angle: %.2f, Isha Angle: %.2f", description, fajrAngle, ishaAngle);
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,35 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String line = "";
+        ArrayList<CalcMethod> arrayList = new ArrayList<>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("./src/main/resources/calc_test.csv"));
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] calcArray = line.split(",");
+            arrayList.add(new CalcMethod(calcArray));
         }
+        /*
+        for(String[] array : arrayList){
+            for(String string : array){
+                System.out.print(string);
+                System.out.print(", ");
+            }
+            System.out.println();
+        }
+        */
+        for(CalcMethod calcMethod : arrayList){
+            System.out.println(calcMethod);
+        }
+        //System.out.println(arrayList);
+        //System.out.println(new File(".").getAbsoluteFile());
+
     }
 }
