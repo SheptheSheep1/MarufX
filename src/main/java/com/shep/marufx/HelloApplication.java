36d440f4bded85d92485fde2ef46447f07bbcbec
Ibrahim Safi <35405069+SheptheSheep1@users.noreply.github.com>
2024-11-13 16:54:40 -0700

    more gui testing

--- src/main/java/com/shep/marufx/HelloApplication.java
+++ src/main/java/com/shep/marufx/HelloApplication.java
@@ -2,7 +2,9 @@ package com.shep.marufx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +12,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent parent = FXMLLoader.<Parent>load(HelloApplication.class.getResource("main_view.fxml"));
+        Scene scene = new Scene(parent);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setResizable(true);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/shep/marufx/MainViewController.java
+++ src/main/java/com/shep/marufx/MainViewController.java
@@ -1,4 +1,39 @@
 package com.shep.marufx;
 
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+
 public class MainViewController {
+    @FXML
+    private Label labell;
+    @FXML
+    private Label fajr;
+    @FXML
+    private Label sunrise;
+    @FXML
+    private Label dhuhr;
+    @FXML
+    private Label asr;
+    @FXML
+    private Label maghrib;
+    @FXML
+    private Label isha;
+
+    @FXML
+    protected void onButtonClick(){
+        labell.setText("Guy Toubes");
+        prayerLabels();
+    }
+
+    @FXML
+    protected void prayerLabels(){
+        fajr.setText("Fajr");
+        sunrise.setText("Sunrise");
+        dhuhr.setText("Dhuhr");
+        asr.setText("Asr");
+        maghrib.setText("Maghrib");
+        isha.setText("Isha");
+    }
+
+
 }
